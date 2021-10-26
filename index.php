346aa8c79604179aaa67616c04a1eae104787933
Alexander <ajveats@gmail.com>
2021-10-26 16:10:38 +0100

    Updated file structure, changed from HTML to PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+
+?>
+<html lang="eng">
+<head>
+    <!-- Website Title -->
+    <title>Wellbeing Website</title>
+
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Wellbeing Tracker Website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Alexander Veats">
+
+    <!-- Stylesheet Styling -->
+    <link rel="stylesheet" href="static/css/index.css"> <!-- Imports the css style sheet index.css -->
+    <link rel="stylesheet" href="static/css/navigationBar.css"> <!-- Imports the css style sheet createAccount.css -->
+
+    <!-- Fonts -->
+</head>
+<body>
+    
+</body>
+</html>
