c9a640fb602b32c86815188b08c94900912d67a4
Alexander <ajveats@gmail.com>
2021-10-28 10:45:58 +0100

    Setting up database and session

---
+++ databaseConnection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "Alexander";
+$password = "alextest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
