9c489bcee41f95d6507d501684c4d4296e5f8c07
Alexander <ajveats@gmail.com>
2021-11-29 18:03:58 +0000

    Birthday Message displayed on users birthday

--- home.php
+++ home.php
@@ -13,24 +13,23 @@ if (!isset($_COOKIE["user_name"])) { // Checks if a users does not have a cookie
 $timeOfDay = date('a');
 $welcomeMessage = '';
 
-if ($timeOfDay == 'am') {
+// create function that splits the users birthday and the current date in month and day, Then compare them and display a message if they are the same
+$userDOB = $_SESSION['user_dob'];
+// echo $userDOB ."<br>";
+$time = strtotime($userDOB);
+$month = date("m", $time);
+$year = date("d", $time);
+$birthday = date("m", $time).date("d", $time);
+
+$today = date('m').date('d');
+
+if ( $birthday === $today ) {
+   $welcomeMessage = 'Happy Birthday ' . $_COOKIE['user_name'] . "! 🎂";
+} elseif ($timeOfDay == 'am'){
     $welcomeMessage =  'Good Morning ' . $_COOKIE["user_name"];
 } else {
     $welcomeMessage = 'Good Afternoon ' . $_COOKIE["user_name"];
 }
-
-
-// create function that splits the users birthday and the current date in month and day, Then compare them and display a message if they are the same
-$userDOB = $_SESSION['user_dob'];
-echo $userDOB ."<br>";
-$time = strtotime($userDOB);
-$month = date("m", $time);
-$year = date("d", $time);
-echo date("m");
-echo date("d");
-
-echo $month;
-echo $year;
 
 ?>
 <html lang="eng">
