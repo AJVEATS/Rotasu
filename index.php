4a11d58d8ff0b71a97be92eb382ff23289659f1d
Alexander <ajveats@gmail.com>
2022-05-20 16:55:01 +0100

    Made the index page more efficient

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<!-- meta tags -->
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 session_start();
+include_once 'static/templates/htmlHeader.php';
 include_once 'includes/php/loginScript.php';
 include_once 'includes/php/createAccountScript.php';
 
@@ -17,28 +14,11 @@ if (isset($_COOKIE["user_id"])) { // Checks if a users does not have a cookie in
     header('Location: home.php'); // Redirects the user to the index page (index.php)
 }
 ?>
-<html lang="eng">
-
-<head>
-    <!-- Website Title -->
-    <title>Wellbeing Website</title>
-
-    <!-- Meta Tags -->
-    <meta charset="UTF-8">
-    <meta name="description" content="Wellbeing Tracker Website">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Alexander Veats">
-
     <!-- Stylesheet Styling -->
     <link rel="stylesheet" href="static/css/index.css"> <!-- Imports the css style sheet index.css -->
     
     <!-- Scripts -->
-
     <script src="static/js/index.js"></script>
-
-    <!-- favicon icon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="static/images/favicon/favicon-32x32.png">
-
 </head>
 
 <body>
--- static/templates/htmlHeader.php
+++ static/templates/htmlHeader.php
@@ -4,6 +4,9 @@
 <html lang="eng">
 
 <head>
+    <!-- Website Title -->
+    <title>Rotasu</title>
+
     <!-- Meta Tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +15,7 @@
     <meta name="author" content="Alexander Veats">
 
     <!-- favicon icon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="static/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="static/images/logo/lotus-32.png">
 
     <!-- Icons -->
     <script src="https://kit.fontawesome.com/dc3d6ef24d.js" crossorigin="anonymous"></script>
