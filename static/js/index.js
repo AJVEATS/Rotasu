8373d97077aed6296be7200746ce55e4d1c387a5
Alexander <ajveats@gmail.com>
2022-05-20 15:51:52 +0100

    Fixed index page bugs

--- static/js/index.js
+++ static/js/index.js
@@ -37,11 +37,3 @@ function showCreateAccountPassword() {
         createAccountPassword.type = "password";
     }
 }
-// function showPassword() {
-//     var x = document.getElementById("password");
-//     if (x.type === "password") {
-//         x.type = "text";
-//     } else {
-//         x.type = "password";
-//     }
-// }
